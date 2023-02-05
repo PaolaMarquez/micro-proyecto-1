a0c6e7e710ba051085905e66ccd8b7a0aa997725
PaolaMarquez <paola.marquez@correo.unimet.edu.ve>
2023-02-05 09:46:02 -0400

    about_me

--- main.js
+++ main.js
@@ -30,9 +30,10 @@ const info = [
 
 window.onload = function () {
     const IMAGENES = [
-        'https://concepto.de/wp-content/uploads/2018/08/monta%C3%B1as-e1533762816593.jpg',
-        'https://img.freepik.com/vector-gratis/parque-infantil-blanco-toboganes-escena_1308-53112.jpg?w=2000',
-        'https://www.hola.com/imagenes/decoracion/20211230202142/cultivar-palmeras-exterior-mc/1-36-318/cuidados-palmeras-exterior-e.jpg'
+        'https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y29kaW5nfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+        'https://images.unsplash.com/opengraph/1x1.png?auto=format&fit=crop&w=1200&h=630&q=60&mark-w=64&mark-align=top%2Cleft&mark-pad=50&blend-w=1&mark=https%3A%2F%2Fimages.unsplash.com%2Fopengraph%2Flogo.png&blend=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1610563166150-b34df4f3bcd6%3Fcrop%3Dfaces%252Cedges%26cs%3Dtinysrgb%26fit%3Dcrop%26fm%3Djpg%26ixid%3DMnwxMjA3fDB8MXxzZWFyY2h8Mnx8c29mdHdhcmUlMjBlbmdpbmVlcnxlbnwwfHx8fDE2NzU0MzgzNzY%26ixlib%3Drb-4.0.3%26q%3D60%26w%3D1200%26auto%3Dformat%26h%3D630%26mark-w%3D750%26mark-align%3Dmiddle%252Ccenter%26blend-mode%3Dnormal%26blend-alpha%3D10%26mark%3Dhttps%253A%252F%252Fimages.unsplash.com%252Fopengraph%252Fsearch-input.png%253Fauto%253Dformat%2526fit%253Dcrop%2526w%253D750%2526h%253D84%2526q%253D60%2526txt-color%253D000000%2526txt-size%253D40%2526txt-align%253Dmiddle%25252Cleft%2526txt-pad%253D80%2526txt-width%253D660%2526txt-clip%253Dellipsis%2526txt%253Dsoftware%252520engineer%26blend%3D000000',
+        './carrusel1.1.png',
+        
     ];
     const TIEMPO_INTERVALO_MILESIMAS_SEG = 2000;
     let posicionActual = 0;
@@ -95,7 +96,7 @@ googleButton.addEventListener('click', () =>{
 
     resultsBoxElem.innerHTML=`
     <divx>
-        <p class="titulo">${info[0].name}</p>
+        <p class="titulo sub-titulo">${info[0].name}</p>
         <div class="results">
         <button type="button" class="value"> Tecnología: ${info[0].tecnology}</button>
         <button type="button" class="value"> Fecha: ${info[0].date}</button>
@@ -112,7 +113,7 @@ facebookButton.addEventListener('click', () =>{
 
     resultsBoxElem.innerHTML=`
     <div>
-        <p class="titulo">${info[1].name}</p>
+        <p class="titulo sub-titulo">${info[1].name}</p>
         <div class="results">
         <button type="button" class="value"> Tecnología: ${info[1].tecnology}</button>
         <button type="button" class="value"> Fecha: ${info[1].date}</button>
@@ -129,7 +130,7 @@ instagramButton.addEventListener('click', () =>{
 
     resultsBoxElem.innerHTML=`
     <div>
-        <p class="titulo">${info[2].name}</p>
+        <p class="titulo sub-titulo">${info[2].name}</p>
         <div class="results">
         <button type="button" class="value"> Tecnología: ${info[2].tecnology}</button>
         <button type="button" class="value"> Fecha: ${info[2].date}</button>
